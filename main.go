4a0a4127eee9d18165604c0cf8f3ab320991f7a5
Chintsai Hwo <jcglqmoyx@gmail.com>
2024-02-23 17:34:16 +0800

    update

--- main.go
+++ main.go
@@ -46,6 +46,22 @@ func generateSecretKey(s string) string {
 	return secretKey
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	logFile, _ := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -58,6 +74,7 @@ func main() {
 		return
 	}
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	r.POST("/", func(c *gin.Context) {
 		var dto RequestDTO
 		c.ShouldBindJSON(&dto)
